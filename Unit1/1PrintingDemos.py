bea5f2a67832ca250898cd89aa232894989a7db1
Daniel Scislowski <mr.scislowski@gmail.com>
2024-03-28 12:11:14 -0500

    did printing demos

---
+++ Unit1/1PrintingDemos.py
@@ -0,0 +1,20 @@
+# print("Hello world")
+# does work
+
+# print(Hello World)
+# doesn't work - need quotes around text
+
+# print("a")
+# print("bc")
+# print("de")
+# you can print lots of lines in a row
+
+# print(2 + 5)
+# print(3 * 3)
+# print(2 + 2 * 10)
+# print("2 + 2 * 10")
+
+# print("Hours in a year:")
+# print(365*24) # 365 days, 24 hours in each day
+
+print('Polonius advises, "To thine own self be true."')

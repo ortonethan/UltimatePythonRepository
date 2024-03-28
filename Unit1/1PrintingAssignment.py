1faaee5f38954da8db76e9e09481787d3a640ec0
EthanOrton <26ethanorton@usd266.com>
2024-03-28 12:11:19 -0500

    did printing assignment

--- Unit1/1PrintingAssignment.py
+++ Unit1/1PrintingAssignment.py
@@ -1,21 +1,26 @@
 print("########## 1.1.1 ##########")
-# your code for exercise 1.1.1 goes here
+print("😊")
 
 print("########## 1.1.2 ##########")
 # modify the code below for exercise 1.1.2
-print("Doc")
-print("Grumpy")
-print("Sleepy")
 print("Bashful")
-print("Happy")
-print("Sneezy")
+print("Doc")
 print("Dopey")
+print("Grumpy")
+print("Happy")
+print("Sleepy")
+print("Sneezy")
 
 print("########## 1.1.3 ##########")
 # your code for exercise 1.1.3 goes here
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
 
 print("########## 1.1.4 ##########")
 # your code for exercise 1.1.4 goes here
-
+print(365*24*60) # 365 days, 24 hours in each day
 print("########## 1.1.5 ##########")
 # your code for exercise 1.1.5 goes here
+print('print("hello world")')

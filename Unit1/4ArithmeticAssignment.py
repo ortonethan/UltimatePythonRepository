6fc6933e7538361064c236212de11124372b5b6d
Daniel Scislowski <mr.scislowski@gmail.com>
2024-03-29 14:09:34 -0500

    Revert "Ready to present 1.4"
    This reverts commit 94b531770dde6ec63f5766076bd2116dd445876f.

---
+++ Unit1/4ArithmeticAssignment.py
@@ -0,0 +1,23 @@
+print("########## 1.4.1 ##########")
+
+print("########## 1.4.2 ##########")
+
+print("########## 1.4.3 ##########")
+
+print("########## 1.4.4 ##########")
+# Fix the code
+number = int(input("Please type in the first number: "))
+number = int(input("Please type in the second number: "))
+number = int(input("Please type in the third number: "))
+
+product = number * number * number
+
+print("The product is", product)
+
+print("########## 1.4.5 ##########")
+
+print("########## 1.4.6 ##########")
+
+print("########## 1.4.7 ##########")
+
+print("########## 1.4.8 ##########")

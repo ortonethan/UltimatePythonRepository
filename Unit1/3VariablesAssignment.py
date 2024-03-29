b33c3738df773127b4578af914567d67feea64aa
EthanOrton <26ethanorton@usd266.com>
2024-03-29 12:38:05 -0500

    did assignment

--- Unit1/3VariablesAssignment.py
+++ Unit1/3VariablesAssignment.py
@@ -11,24 +11,26 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
+print("my name is",name+", I am",age,"years old")
 print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print("-",skill1,"("+level1+")")
+print("-",skill2,"("+level2+")")
+print("-",skill3,"("+level3+")")
+print("I am looking for a job with a salary of", str(lower)+"-"+str(upper),"dollars per month")
 
 
 print("########## 1.3.2 ##########")
 x = 27
 y = 15
-
-
+print(x,"+",y,"=", x+y)
+print(x,"-",y,"=", x-y)
+print(x,"*",y,"=", x*y)
+print(x,"/",y,"=", x/y)
 print("########## 1.3.3 ##########")
-print(5)
-print(" + ")
-print(8)
-print(" - ")
-print(4)
-print(" = ")
+print(5, end="")
+print(" + ", end="")
+print(8, end="")
+print(" - ", end="")
+print(4, end="")
+print(" = ", end="")
 print(5 + 8 - 4)

1e213a44cbfe794a4e8a176f425374ca4d60ff76
EthanOrton <26ethanorton@usd266.com>
2024-04-09 12:09:59 -0500

    did half

--- Unit1/5ConditionalsAssignment.py
+++ Unit1/5ConditionalsAssignment.py
@@ -1,13 +1,31 @@
 
 print("########## 1.5.1 ##########")
-
-print("########## 1.5.2 ##########")
-
-print("########## 1.5.3 ##########")
-
-print("########## 1.5.4 ##########")
-
+#year = int(input("Please type in a number:"))
+#if year == int(1984):
+#    print("Orwell")
+#print("########## 1.5.2 ##########")
+#number = int(input("Please type in a number:"))
+#if number < 0 :
+#    number = number*-1
+#print("The absolute value of this number is", number)
+#print("########## 1.5.3 ##########")
+#Name = (input("What is your name? "))
+#if Name != "jerry":
+#    portions = int(input("How many portions of soup?"))
+#    print("The total cost is", portions*5.9)
+#print("Next Please!")
+#print("########## 1.5.4 ##########")
+#number = int(input("Type in a number:"))
+#if number < 1000:
+#    print("This number is less than 1000")
+    # if number < 100:
+    #     print("This number is less than 100")
+    #     if number < 10:
+    #         print("This number is less than 10")
+#print("Thank you!")
 print("########## 1.5.5 ##########")
+number1 = int(input("Number 1:"))
+number2 = int(input("Number 2:"))
 
 print("########## 1.5.6 ##########")
 
@@ -27,3 +45,4 @@ if points >= 100:
 print("You now have", points, "points")
 
 print("########## 1.5.9 ##########")
+print("What is the weather forcast tomorrow?")

99fdaec5277e5f6ca34b4446cfcea065aa0412b8
EthanOrton <26ethanorton@usd266.com>
2024-04-10 12:31:56 -0500

    did some work

--- Unit2/1MoreConditionalsAssignment.py
+++ Unit2/1MoreConditionalsAssignment.py
@@ -1,11 +1,27 @@
 # print("########## 2.1.1 ##########")
-
+#word = input("PLease type in a word: ")
+#print("there are " ,len(word), " letters in the word " , word)
+#print("Thank you!")
 # print("########## 2.1.2 ##########")
-
+#number = float(input("Please type in a number: "))
+#print("Integer part: ", int(number))
+#print("Decimal part: ", (float(number)-(int(number))))
 # print("########## 2.1.3 ##########")
-
+#agetovote = input("How old are you? ")
+#if int(agetovote) < 18:
+#    print("you are not able to vote")
+#elif int(agetovote) >= 18:
+#    print("You may vote!")
 # print("########## 2.1.4 ##########")
-
+#n1 = int(input("Please type in the first number: "))
+#n2 = int(input("Please type in the second number: "))
+#if n1 > n2:
+#    print("the greater number was: ", n1)
+#if n1 < n2:
+#    print("the greater number was: ", n2)
+#if n1 == n2:
+#    print("The numbers are equal!: ", n1)
 # print("########## 2.1.5 ##########")
+print("person 1:")
 
 # print("########## 2.1.6 ##########")

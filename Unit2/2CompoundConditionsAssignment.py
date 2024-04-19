75640b0ba2e5a0b6cc56ada5be06311df8cc6e8e
EthanOrton <26ethanorton@usd266.com>
2024-04-19 11:17:30 -0500

    almostdone

--- Unit2/2CompoundConditionsAssignment.py
+++ Unit2/2CompoundConditionsAssignment.py
@@ -1,9 +1,25 @@
 # print("########## 2.2.1 ##########")
-
+number = int(input("What is your age: "))
+if number >= 5:
+    print("Ok, you are", number ,"years old")
+elif number <= 5:
+    print("I suspect you can't write quite yet...")
+elif number <= 0:
+    print("that must be a mistake")
 # print("########## 2.2.2 ##########")
-
+name = input("Please type in your name: ")
+if name == "Morty" or name == "Ferdie" :
+    print("I think you are one of Mickey mouses nephews")
+elif name == "Huey" or name == "Dewey" or name == "Louie" :
+    print("I think you might be one of Donald Duck's nephews.")    
+else : print("You're not a nephew of any character I know of.")
 # print("########## 2.2.3 ##########")
-
+if number >= 59:
+    print("F")
+elif number <= 60:
+    print("I suspect you can't write quite yet...")
+elif number <= 0:
+    print("that must be a mistake")
 # print("########## 2.2.4 ##########")
 
 # print("########## 2.2.5 ##########")

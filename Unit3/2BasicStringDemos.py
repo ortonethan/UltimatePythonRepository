a9992a991e7bd01ae2f2d66af87d94c6ef2dc8ee
Daniel Scislowski <mr.scislowski@gmail.com>
2024-04-22 12:03:41 -0500

    did 3.2 demos

---
+++ Unit3/2BasicStringDemos.py
@@ -0,0 +1,37 @@
+# joining strings with the "+" operator
+
+# s1 = "py"
+# s2 = "thon"
+
+# combined = s1 + s2
+
+# print("Combined string is: ")
+# print(combined)
+
+# repeating strings with the "*" operator
+
+# word = "banana"
+# repeated = word * 3
+
+# print(repeated)
+
+# len of a string to underline a word
+# word = input("Please type in a word: ")
+# print(word)
+# numletters = len(word)
+# print("-" * numletters)
+
+# indexing to get individual characters
+word = "demo"
+
+# get 1st, 2nd, 3rd... letters
+letter = word[0]
+print("The letter is:", letter)
+
+# get last, 2nd to last, etc letters
+letter = word[ len(word) - 1 ]
+print("The letter is", letter)
+
+# do this using negative indexing
+letter = word[-1]
+print("The letter is", letter)

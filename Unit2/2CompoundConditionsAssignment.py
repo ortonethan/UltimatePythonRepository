7bd31bb72fe4ceadd6b194022608bf0d935ee46f
EthanOrton <26ethanorton@usd266.com>
2024-04-22 11:57:20 -0500

    almost done

--- Unit2/2CompoundConditionsAssignment.py
+++ Unit2/2CompoundConditionsAssignment.py
@@ -1,27 +1,34 @@
 # print("########## 2.2.1 ##########")
-number = int(input("What is your age: "))
-if number >= 5:
-    print("Ok, you are", number ,"years old")
-elif number <= 5:
-    print("I suspect you can't write quite yet...")
-elif number <= 0:
-    print("that must be a mistake")
-# print("########## 2.2.2 ##########")
-name = input("Please type in your name: ")
-if name == "Morty" or name == "Ferdie" :
-    print("I think you are one of Mickey mouses nephews")
-elif name == "Huey" or name == "Dewey" or name == "Louie" :
-    print("I think you might be one of Donald Duck's nephews.")    
-else : print("You're not a nephew of any character I know of.")
-# print("########## 2.2.3 ##########")
+# number = int(input("What is your age: "))
+# if number >= 5:
+#     print("Ok, you are", number ,"years old")
+# elif number <= 5:
+#     print("I suspect you can't write quite yet...")
+# elif number <= 0:
+#     print("that must be a mistake")
+# # print("########## 2.2.2 ##########")
+# name = input("Please type in your name: ")
+# if name == "Morty" or name == "Ferdie" :
+#     print("I think you are one of Mickey mouses nephews")
+# elif name == "Huey" or name == "Dewey" or name == "Louie" :
+#     print("I think you might be one of Donald Duck's nephews.")    
+# else : print("You're not a nephew of any character I know of.")
+# # print("########## 2.2.3 ##########")
+number = int(input("Type in percent: "))
 if number >= 59:
     print("F")
-elif number <= 60:
-    print("I suspect you can't write quite yet...")
-elif number <= 0:
-    print("that must be a mistake")
+elif number >= 60 and number <= 69:
+    print("D")
+elif number >= 70 and number <= 79:
+    print("C")
+elif number >= 80 and number <= 89:
+    print("B")
+elif number >= 90 and number <= 100:
+    print("A")
+elif number <=0 or number >> 100: 
+    print("impossible!") 
 # print("########## 2.2.4 ##########")
-
+    
 # print("########## 2.2.5 ##########")
 
 # print("########## 2.2.6 ##########")

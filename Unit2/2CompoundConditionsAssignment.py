8b2a31b29945dcde39074de6addc2a53758820ff
EthanOrton <26ethanorton@usd266.com>
2024-04-24 10:39:21 -0500

    worked on them

--- Unit2/2CompoundConditionsAssignment.py
+++ Unit2/2CompoundConditionsAssignment.py
@@ -14,21 +14,41 @@
 #     print("I think you might be one of Donald Duck's nephews.")    
 # else : print("You're not a nephew of any character I know of.")
 # # print("########## 2.2.3 ##########")
-number = int(input("Type in percent: "))
-if number >= 59:
-    print("F")
-elif number >= 60 and number <= 69:
-    print("D")
-elif number >= 70 and number <= 79:
-    print("C")
-elif number >= 80 and number <= 89:
-    print("B")
-elif number >= 90 and number <= 100:
-    print("A")
-elif number <=0 or number >> 100: 
-    print("impossible!") 
+# number = int(input("Type in percent: "))
+# if number >= 59:
+#     print("F")
+# elif number >= 60 and number <= 69:
+#     print("D")
+# elif number >= 70 and number <= 79:
+#     print("C")
+# elif number >= 80 and number <= 89:
+#     print("B")
+# elif number >= 90 and number <= 100:
+#     print("A")
+# elif number <=0 or number >> 100: 
+#     print("impossible!") 
 # print("########## 2.2.4 ##########")
-    
+# number = int(input("Number: "))
+# if number %3 == 0:
+#     print("Fizz")
+# if number %5 == 0:
+#     print("Buzz")
+# elif number %5 == 0 and number %3 == 0:
+#     print("FizzBuzz")
+# else:
+#     print("")
 # print("########## 2.2.5 ##########")
-
+# ly = int(input("Please type in a year: "))
+# if ly %4 == 0 and (ly %400 == 0 or ly %100 != 0):
+#     print("that year is a leap year. ")
+# else:
+#     print("that year is not a leap year")
 # print("########## 2.2.6 ##########")
+l1 = (input("1st letter: "))
+l2 = (input("2nd letter: "))
+l3 = (input("3rd letter: "))
+if l1 > l2 and l1 < l3:
+    print("the letter in the middle is ", l1)
+if l2 > l1 and l2 < l3:
+    print("the letter in the middle is ", l1)
+
--- Unit2/3WhileTrueLoopsAssignment.py
+++ Unit2/3WhileTrueLoopsAssignment.py
@@ -1,22 +1,47 @@
 # ========== 2.3.1 ==========
-
-
-# ========== 2.3.2 ==========
-
-
-# ========== 2.3.3 ==========
-# number = 5
-# print("Countdown!")
 # while True:
-#   print(number)
-#   number = number - 1
-#   if number > 0:
-#     break
-#
-# print("Now!")
+#     answer = (input("Please type in a number, -1 to quit: "))
+
+#     if answer == "no":
+#         break
+
+#     print("ok then")
+
+# print("Thanks and bye!")
+
+# # ========== 2.3.2 ==========
+# from math import sqrt
+# while True:
+#     number = int(input("Please type in a number: "))
+
+#     if number < 0:
+#         print("invalid number")
+#     elif number > 0:
+#         print(sqrt(number))
+#     elif number == 0:
+#         break
+#     print("Exiting...")
+# ========== 2.3.3 ==========
+number = 5
+print("Countdown!")
+while True:
+  print(number)
+  number = number - 1
+  if number > 0:
+    break
+
+print("Now!")
 
 # ========== 2.3.4 ==========
+while True:
+    p1 = (input("Password: "))
+    p2 = (input("Repeat Password: "))
 
+    if p1 == p2:
+        print("User account created!")
+    
+        break
+    print("Exiting...")
 
 # ========== 2.3.5 ==========
 

e350d4221621b42549696ee7930e3a9b85b80998
EthanOrton <26ethanorton@usd266.com>
2024-05-15 11:44:29 -0500

    did 3.2

--- Unit3/2BasicStringAssignment.py
+++ Unit3/2BasicStringAssignment.py
@@ -1,15 +1,27 @@
 # ========== 3.2.1 ==========
-
-
+w = input("Please type in a string: ")
+a = int(input("Please type in an amount: "))
+print((w*a))
 # ========== 3.2.2 ==========
-
-
+s1 = input("Please type in string 1: ")
+s2 = input("Please type in string 1: ")
+if len(s1) > len(s2):
+    print(s1, "is longer")
+elif len(s2) > len(s1):
+    print(s2, "is longer")
+else:
+    print("strings are equally long")
 # ========== 3.2.3 ==========
-
-
+w = input("Please type in a string")
+p = len(w)-1
+while p >= 0:
+    print(w[p])
+    p -= 1
 # ========== 3.2.4 ==========
-
-
+w = input("Please type in a string")
+p = len(w)-1
+if len(w) >= 2:
+    
 # ========== 3.2.5 ==========
 
 

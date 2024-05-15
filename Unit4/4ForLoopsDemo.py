024e5f3daf4c780d4b094088990562bccaad25d6
Daniel Scislowski <mr.scislowski@gmail.com>
2024-05-15 11:42:59 -0500

    Did 4.4 demos

---
+++ Unit4/4ForLoopsDemo.py
@@ -0,0 +1,38 @@
+items = [2, 5, 1, 2, 3]
+
+# print all items on a separate line using while loop
+# index = 0
+# while index < len(items):
+#     print(items[index])
+#     index += 1
+
+# print all items on a separate line using for loop
+# for number in items:
+#     print(number)
+
+
+# show that this works with strings too
+# word = "example"
+
+# for letter in word:
+#     print(letter)
+
+# count from 0 to 5 using range(...)
+
+# for number in range(0, 6):
+#     print(number)
+
+
+# count from 2 to 7 using range(..., ...)
+# for number in range(2, 8):
+#     print(number)
+
+# count by 2's: 3, 5, 7, 9 using range(..., ..., ...)
+
+# for number in range(3, 10, 2):
+#     print(number)
+
+# count down from 5 to 1 using range(..., ..., ...)
+
+# for number in range(5, 0, -1):
+#     print(number)

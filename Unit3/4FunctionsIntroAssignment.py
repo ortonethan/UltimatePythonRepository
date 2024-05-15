84be7d4d6dab383c6c9a553860a1a7802329635c
ortonethan <ethan10orton@gmail.com>
2024-05-14 19:02:11 -0500

    did at home

--- Unit3/4FunctionsIntroAssignment.py
+++ Unit3/4FunctionsIntroAssignment.py
@@ -1,8 +1,24 @@
 # ========== 3.4.1 ==========
+def seven_dwarves():
+    print("Bashful")
+    print("Doc")
+    print("Dopey")
+    print("Grumpy")
+    print("Happy")
+    print("Sleepy")
+    print("Sneezy")
 
+seven_dwarves()
 
-# ========== 3.4.2 ==========
-
+# ========= 3.4.2 ==========
+def first_character(x):
+    print(x[0])
+first_character("python")
+first_character("yellow")
+first_character("tomorrow")
+first_character("heliotrope")
+first_character("open")
+first_character("night")
 
 # ========== 3.4.3 ==========
 

60babd878db1a63b09d999351cdfd885c3f473f9
ortonethan <ethan10orton@gmail.com>
2024-05-15 23:23:59 -0500

    3.1 done

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,24 +1,42 @@
-# ========== 3.1.1 ==========
-n = 30
-while n > 2:
-    print(n)
-    n = n -2
-
-# ========== 3.1.2 ==========
-
+# # ========== 3.1.1 ==========
+# n = 30
+# while n > 2:
+#     print(n)
+#     n = n -2
+# # ========== 3.1.2 ==========
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
+# while number != 0:
+#     print(number)
+#     number = number +1
 # print("Go!")
-
-# ========== 3.1.3 ==========
-
-
-# ========== 3.1.4 ==========
-
-
-# ========== 3.1.5 ==========
-
-
-# ========== 3.1.6 ==========
+# # ========== 3.1.3 ==========
+# l = int(input("upper limit: "))
+# n = 1
+# while l > n:
+#     print(n)
+#     n = n + 1
+# # ========== 3.1.4 ==========
+# l = int(input("upper limit: "))
+# n = 0
+# while l > (2**n):
+#     print(n)
+#     n = n + 1
+# # ========== 3.1.5 ==========
+# l = int(input("upper limit: "))
+# b = int(input("base: "))
+# n = 0
+# while l >= (b**n):
+#     print(n)
+#     n = n + 1
+# # ========== 3.1.6 ==========
+# l = int(input("upper limit: "))
+# n = 1
+# t = 0
+# while True:
+#     if t < l:
+#         t = t + n
+#         n = n + 1
+#     else:
+#         print(t)
+#         break
--- Unit3/2BasicStringAssignment.py
+++ Unit3/2BasicStringAssignment.py
@@ -1,57 +1,57 @@
-# ========== 3.2.1 ==========
-w = input("please type in a string: ")
-a = int(input("please type in an amount: "))
-print((w*a))
-# ========== 3.2.2 ==========
-s1 = input("please type in string 1: ")
-s2 = input("please type in string 1: ")
-if len(s1) > len(s2):
-    print(s1, "is longer")
-elif len(s2) > len(s1):
-    print(s2, "is longer")
-else:
-    print("strings are equally long")
-# ========== 3.2.3 ==========
-w = input("please type in a string")
-p = len(w)-1
-while p >= 0:
-    print(w[p])
-    p -= 1
-# ========== 3.2.4 ==========
-w = input("please type in a string")
-p = len(w)-1
-if len(w) >= 2:
-    if(w[p - 1]) == (w[1]):
-        print("the second and second to last character are", w[1])
-    else:
-        print("the second and second to last characters are different")
-# ========== 3.2.5 ==========
-w = int(input("width: "))
-print("#"*w)
-# ========== 3.2.6 ==========
-w = int(input("width: "))
-h = int(input("height: "))
-while h > 0:
-    print("#"*w)
-    h -= 1
-# ========== 3.2.7 ==========
-while True:
-    s1 = input("please type in a string: ")
-    if s1 != "":
-        print(s1)
-        print("-"*len(s1))
-    else:
-        break
-# ========== 3.2.8 ==========
-s1 = input("please type in a string: ")
-print("*"*(20-len(s1)))
-# ========== 3.2.9 ==========
-w = input("word: ")
-l = int(len(w)/2)
-print("")
-print("*"*30)
-if len(w) % 2 == 0:
-    print("*"+(" "*(14-l)) + w + (" "*(14-l)) + "*")
-else:
-    print("*"+(" "*(14-l)) + w + (" "*(13-l)) + "*")
-print("*"*30)
+# # ========== 3.2.1 ==========
+# w = input("please type in a string: ")
+# a = int(input("please type in an amount: "))
+# print((w*a))
+# # ========== 3.2.2 ==========
+# s1 = input("please type in string 1: ")
+# s2 = input("please type in string 1: ")
+# if len(s1) > len(s2):
+#     print(s1, "is longer")
+# elif len(s2) > len(s1):
+#     print(s2, "is longer")
+# else:
+#     print("strings are equally long")
+# # ========== 3.2.3 ==========
+# w = input("please type in a string")
+# p = len(w)-1
+# while p >= 0:
+#     print(w[p])
+#     p -= 1
+# # ========== 3.2.4 ==========
+# w = input("please type in a string")
+# p = len(w)-1
+# if len(w) >= 2:
+#     if(w[p - 1]) == (w[1]):
+#         print("the second and second to last character are", w[1])
+#     else:
+#         print("the second and second to last characters are different")
+# # ========== 3.2.5 ==========
+# w = int(input("width: "))
+# print("#"*w)
+# # ========== 3.2.6 ==========
+# w = int(input("width: "))
+# h = int(input("height: "))
+# while h > 0:
+#     print("#"*w)
+#     h -= 1
+# # ========== 3.2.7 ==========
+# while True:
+#     s1 = input("please type in a string: ")
+#     if s1 != "":
+#         print(s1)
+#         print("-"*len(s1))
+#     else:
+#         break
+# # ========== 3.2.8 ==========
+# s1 = input("please type in a string: ")
+# print("*"*(20-len(s1)))
+# # ========== 3.2.9 ==========
+# w = input("word: ")
+# l = int(len(w)/2)
+# print("")
+# print("*"*30)
+# if len(w) % 2 == 0:
+#     print("*"+(" "*(14-l)) + w + (" "*(14-l)) + "*")
+# else:
+#     print("*"+(" "*(14-l)) + w + (" "*(13-l)) + "*")
+# print("*"*30)
